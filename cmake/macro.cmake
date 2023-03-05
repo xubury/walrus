953abe8e9fe150ffe182b47dc13549272e3dd8ff
xubury <xubury97@gmail.com>
2023-03-05 21:43:29 +0800

    * add demo program

---
+++ cmake/macro.cmake
@@ -0,0 +1,16 @@
+function(file_download path url)
+  get_filename_component(filename ${path} NAME)
+  message(STATUS "Downloading ${filename} from ${url}")
+  file(
+    DOWNLOAD 
+    "${url}"
+    "${path}"
+    TLS_VERIFY ON
+    STATUS DOWNLOAD_RESULT)
+  list(GET DOWNLOAD_RESULT 0 DOWNLOAD_RESULT_CODE)
+  if(NOT DOWNLOAD_RESULT_CODE EQUAL 0)
+    file(REMOVE ${path})
+    message(FATAL_ERROR "Download failed with error: ${DOWNLOAD_RESULT}")
+  endif()
+  message(STATUS "Download finished")
+endfunction()
