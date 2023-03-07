5512b70c2a9fbb47fadccb0b11a5999385deb3da
xubury <xubury97@gmail.com>
2023-03-07 23:21:42 +0800

    * basic printf

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -1,15 +1,13 @@
+#define __wasi__
+
 #include <stdint.h>
 #include <string.h>
+#include <stdio.h>
 
-extern "C" void console_log(uint32_t value);
 extern uint8_t memory;
 
-extern "C" void run()
-{
-    console_log(123);
-    char *ptr = (char *)&memory;
-    const char *str = "hello world";
-    for (uint8_t i = 0; i < strlen(str); ++i) {
-        ptr[i] = str[i];
-    }
+extern "C" void run() {
+  const char *str = "hello world";
+  size_t len = strlen(str);
+  printf("hello world\n");
 }
