3c54532ba34a250ab71b1ee1146bbf794781b277
xubury <xubury97@gmail.com>
2023-03-08 20:14:56 +0800

    * Organize cmake and clang-format

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -3,12 +3,13 @@
 #include <stdio.h>
 #include <string.h>
 
-extern "C" int wasm_main(int argc, char *argv[]) {
-  printf("This is a string\n");
-  printf("Hello world, argc:%d, argv[0]:%s\n", argc, argv[0]);
-  printf("sinf:%f\n", sinf(1.0));
-  char *ptr = (char *)malloc(100);
-  ptr[0] = 122;
-  printf("malloc:0x%lx ptr[0]:%d\n", (intptr_t)ptr, ptr[0]);
-  return 0;
+extern "C" int wasm_main(int argc, char *argv[])
+{
+    printf("This is a string\n");
+    printf("Hello world, argc:%d, argv[0]:%s\n", argc, argv[0]);
+    printf("sinf:%f\n", sinf(1.0));
+    char *ptr = (char *)malloc(100);
+    ptr[0] = 122;
+    printf("malloc:0x%lx ptr[0]:%d\n", (intptr_t)ptr, ptr[0]);
+    return 0;
 }
