ec2c6a6c3226cc05695f4c102905017e407e82e0
xubury <xubury97@gmail.com>
2023-03-20 09:43:04 +0800

    * fs test

--- web/server.js
+++ web/server.js
@@ -1,14 +1,68 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
+var fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-// TODO:filesystem emu
-app.get("/file/:name", function(req, res) {
-    // console.log(req.params.name);
-    res.send("world");
-})
+// create application/json parser
+app.use(bodyParser.json());
+
+var fd_cursors = [];
+var fd_payloads = [];
+app.get("/fd_open", function (req, res) {
+    const filename = path.join(__dirname, "files", req.query.filename);
+    try {
+        const fd = fs.openSync(filename);
+        console.log("[TRACE] fd_open on fd:" + fd);
+        res.send(JSON.stringify({ fd: fd }));
+        fd_cursors[fd] = 0;
+        fs.read(fd, function (err, bytesRead, buffer) {
+            fd_payloads[fd] = buffer;
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(JSON.stringify({ error: error }));
+    }
+});
+
+app.get("/fd_read", function (req, res) {
+    const fd = Number(req.query.fd);
+    const length = Number(req.query.length);
+    console.log("[TRACE] fd_read on fd: " + fd + " length: " + length);
+
+    var payload = fd_payloads[fd];
+    var cursor = fd_cursors[fd];
+    var read = Math.min(length, payload.length - cursor);
+    payload = payload.subarray(cursor, cursor + read);
+    fd_cursors[fd] += read;
+    res.send(JSON.stringify({payload:payload}));
+});
+
+app.get("/fd_close", function (req, res) {
+    const fd = Number(req.query.fd);
+    console.log("[TRACE] fd_close on fd: " + fd);
+    fs.closeSync(fd);
+});
+
+app.get("/fd_seek", function (req, res) {
+    const fd = Number(req.query.fd);
+    const offset = Number(req.query.offset);
+    const whence = Number(req.query.whence);
+    console.log(
+        "[TRACE] fd_seek on fd: " +
+            fd +
+            " offset: " +
+            offset +
+            " whence: " +
+            whence
+    );
+    if (whence == 0) fd_cursors[fd] = offset; //set
+    if (whence == 1) fd_cursors[fd] += offset; //cur
+    if (whence == 2) fd_cursors[fd] = fd_payloads[fd].length - offset; //end
+    res.sendStatus(200);
+});
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "res/index.html"));
--- web/src/wasm.js
+++ web/src/wasm.js
@@ -5,12 +5,57 @@ var res = await fetch("/wasm");
 var wasmBytes = await res.arrayBuffer();
 await initSys(wasmBytes, loadLibraries);
 
-// TODO:filesystem emu
-res = await fetch("/file/hello");
-var enc = new TextDecoder("utf-8");
-console.log(enc.decode(await res.arrayBuffer()))
+async function fd_open(filename) {
+    // open a file, return a fd
+    const res = await fetch(
+        "/fd_open?" + new URLSearchParams({ filename: filename })
+    );
+    const body = await res.json();
+    const fd = body.fd;
+    const err = body.error;
+    if (fd == null) {
+        console.log(err)
+        fd = 0;
+    }
+    return fd;
+}
+
+async function fd_read(fd, length) {
+    var res = await fetch(
+        "/fd_read?" +
+            new URLSearchParams({
+                fd: fd,
+                length: length,
+            })
+    );
+    var body = await res.json();
+    console.log(new Uint8Array(body.payload.data));
+}
+
+// close file with a fd
+async function fd_close(fd) {
+    await fetch("/fd_close?" + new URLSearchParams({ fd: fd }));
+}
+
+async function fd_seek(fd, offset, whence) {
+    await fetch(
+        "/fd_seek?" +
+            new URLSearchParams({ fd: fd, offset: offset, whence: whence })
+    );
+}
+
+const SEEK_SET = 0;
+const SEEK_CUR = 1;
+const SEEK_END = 2;
+
+var fd = await fd_open("favicon.ico");
+if (fd != null) {
+    await fd_read(fd, 21, 10);
+    await fd_seek(fd, 10, SEEK_SET);
+    await fd_close(fd);
+}
 
 function loadLibraries(env) {
-    console.log("[WASMJS] loadLibraies");
+    console.log("[WAJS] loadLibraies");
     importGl(env);
 }
