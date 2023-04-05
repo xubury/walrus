a646fc024ba20555e98a7ac02b5fded64fd042ee
xubury <xubury97@gmail.com>
2023-04-05 11:38:49 +0800

    * Electron fix

--- web/electron/main.js
+++ web/electron/main.js
@@ -1,42 +1,42 @@
 // main.js
-const { app, BrowserWindow, session } = require('electron')
-const path = require('path')
-const isDev = require('electron-is-dev');
-const os = require('os')
-const waitPort = require('wait-port');
-require("../server.js")
+const { app, BrowserWindow, session } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+const os = require("os");
+const waitPort = require("wait-port");
+require("../server.js");
 
- // on windows
- const reactDevToolsPath = path.join(
-   os.homedir(),
-   '/AppData/Local/Google/Chrome/User Data/Default/Extensions/pdcpmagijalfljmkmjngeonclgbbannb/0.2.5386.0_0'
- )
+// on windows
+const reactDevToolsPath = path.join(
+    os.homedir(),
+    "/AppData/Local/Google/Chrome/User Data/Default/Extensions/pdcpmagijalfljmkmjngeonclgbbannb/0.2.5386.0_0"
+);
 
-async function createWindow () {
+async function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-    })
+    });
 
     const params = {
-      host: 'localhost',
-      port: 8080,
+        host: "localhost",
+        port: 8080,
     };
     await waitPort(params);
     if (isDev) {
-        await session.defaultSession.loadExtension(reactDevToolsPath)
-        mainWindow.webContents.openDevTools()
-    } 
-    mainWindow.loadURL('http://localhost:8080/'); 
+        await session.defaultSession.loadExtension(reactDevToolsPath);
+        mainWindow.webContents.openDevTools();
+    }
+    mainWindow.loadURL("http://localhost:8080/?wasm=rotate_cube.wasm");
 }
 
 app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+    createWindow();
+    app.on("activate", function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") app.quit();
+});
